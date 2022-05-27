3c7844f4af905d83dfc70f0d6f27cb14d1a56ba8
Ernesto <ernesto.rapisarda81@gmail.com>
2022-05-27 16:36:54 +0200

    cazzo

--- src/main/java/it/unical/sadstudents/mediaplayeruid/controller/MainController.java
+++ src/main/java/it/unical/sadstudents/mediaplayeruid/controller/MainController.java
@@ -2,11 +2,37 @@ package it.unical.sadstudents.mediaplayeruid.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToolBar;
 
 public class MainController {
 
     @FXML
+    private Label durationMediaPlayed;
+
+    @FXML
+    private Label nameMediaPlayed;
+
+    @FXML
+    private Slider sliderMediaPlayed;
+
+    @FXML
+    private Label tabAttuale;
+
+    @FXML
+    private Label timeMediaPlayed;
+
+    @FXML
+    private ToolBar toolbarMenu;
+
+    @FXML
     void onHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMediaPlayedSett(ActionEvent event) {
 
     }
 
@@ -16,7 +42,17 @@ public class MainController {
     }
 
     @FXML
+    void onNext(ActionEvent event) {
+
+    }
+
+    @FXML
     void onPlayLists(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPlayPause(ActionEvent event) {
 
     }
 
@@ -26,11 +62,37 @@ public class MainController {
     }
 
     @FXML
+    void onPrevious(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRepeat(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onScreenMode(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onShuffle(ActionEvent event) {
+
+    }
+
+    @FXML
     void onVideoLibrary(ActionEvent event) {
 
     }
+
     @FXML
-    void onSettings(ActionEvent event) {
+    void onVolume(ActionEvent event) {
 
     }
 
