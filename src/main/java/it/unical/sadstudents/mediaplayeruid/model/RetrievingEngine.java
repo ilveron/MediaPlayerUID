e4cf0d7825c738cc7d246d249530570389e46e05
Ernesto <ernesto.rapisarda81@gmail.com>
2022-06-01 13:43:03 +0200

    pd e pm

---
+++ src/main/java/it/unical/sadstudents/mediaplayeruid/model/RetrievingEngine.java
@@ -0,0 +1,15 @@
+package it.unical.sadstudents.mediaplayeruid.model;
+
+import java.io.File;
+
+public class RetrievingEngine {
+    public File retrieveFile(){
+        // TODO: 01/06/2022
+        return null;
+    }
+
+    public File[] retrieveFolder(){
+        // TODO: 01/06/2022
+        return null;
+    }
+}
